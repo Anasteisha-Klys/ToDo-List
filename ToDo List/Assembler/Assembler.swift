1f04a071018158b79307243826a2c4c52cbac323
Anasteisha-Klys <134367935+Anasteisha-Klys@users.noreply.github.com>
2024-11-17 11:51:47 +1000

    merging folders

---
+++ ToDo List/Assembler/Assembler.swift
@@ -0,0 +1,13 @@
+//
+//  Assembler.swift
+//  ToDo List
+//
+//  Created by Анастасия Клюшникова on 17.11.2024.
+//
+
+import Foundation
+
+final class Assembler {
+    private init(){}
+    static let shared = Assembler()
+}
